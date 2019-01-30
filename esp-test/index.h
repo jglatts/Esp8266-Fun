9fe5c21b1b61e5e5383137e44b31b4de161381af
John Glatts <33476958+jglatts@users.noreply.github.com>
2019-01-30 09:30:47 -0500

    Create index.h

---
+++ esp-test/index.h
@@ -0,0 +1,29 @@
+const char page[] PROGMEM = R"=====(
+<HTML>
+  <HEAD>
+      <TITLE>AYYYY</TITLE>
+  </HEAD>
+  <style>
+    #test:hover {
+      color: brown;
+    }
+    h1 {
+      font-size: 5em;
+    }
+    button {
+      font-size: 2.5em;
+    }
+    a {
+      text-decoration: none;
+    }
+  </style>
+<BODY>
+  <CENTER>
+      <h1><B>Motor Control!</B></h1>
+      <br>
+      <button><a href="LEDOn">Motor On</a></button>
+      <button><a href="LEDOff">Motor Off</a></button>
+  </CENTER> 
+</BODY>
+</HTML>
+)=====";
