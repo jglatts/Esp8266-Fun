87f7381af57e72db9cf6852b88aff9a54eca0f9d
John Glatts <33476958+jglatts@users.noreply.github.com>
2019-01-30 10:22:59 -0500

    Update index.h

--- esp-test/index.h
+++ esp-test/index.h
@@ -1,3 +1,5 @@
+
+// html main page
 const char page[] PROGMEM = R"=====(
 <HTML>
   <HEAD>
@@ -6,6 +8,10 @@ const char page[] PROGMEM = R"=====(
   <style>
     #test:hover {
       color: brown;
+    }
+    #color {
+	    color: black;
+      font-size:3em;
     }
     h1 {
       font-size: 5em;
@@ -19,8 +25,81 @@ const char page[] PROGMEM = R"=====(
   </style>
 <BODY>
   <CENTER>
-      <h1><B>Motor Control!</B></h1>
-      <br>
+      <h1><B>Motor Control!!!</B></h1>
+      <p id="color">Motor is Off!</p>
+      <button><a href="LEDOn">Motor On</a></button>
+      <button><a href="LEDOff">Motor Off</a></button>
+  </CENTER> 
+</BODY>
+</HTML>
+)=====";
+
+
+
+/* Motor OFF html page */
+const char off_page[] PROGMEM = R"=====(
+<HTML>
+  <HEAD>
+      <TITLE>AYYYY</TITLE>
+  </HEAD>
+  <style>
+    #test:hover {
+      color: brown;
+    }
+    #color {
+	    color: black;
+      font-size:3em;
+    }
+    h1 {
+      font-size: 5em;
+    }
+    button {
+      font-size: 2.5em;
+    }
+    a {
+      text-decoration: none;
+    }
+  </style>
+<BODY>
+  <CENTER>
+      <h1><B>Motor Control!!!</B></h1>
+      <p id="color">Motor is Off!</p>
+      <button><a href="LEDOn">Motor On</a></button>
+      <button><a href="LEDOff">Motor Off</a></button>
+  </CENTER> 
+</BODY>
+</HTML>
+)=====";
+
+
+/* Motor ON html page */
+const char on_page[] PROGMEM = R"=====(
+<HTML>
+  <HEAD>
+      <TITLE>AYYYY</TITLE>
+  </HEAD>
+  <style>
+    #test:hover {
+      color: brown;
+    }
+    #color {
+	    color: red;
+      font-size:3em;
+    }
+    h1 {
+      font-size: 5em;
+    }
+    button {
+      font-size: 2.5em;
+    }
+    a {
+      text-decoration: none;
+    }
+  </style>
+<BODY>
+  <CENTER>
+      <h1><B>Motor Control!!!</B></h1>
+      <p id="color">Motor is On!</p>
       <button><a href="LEDOn">Motor On</a></button>
       <button><a href="LEDOff">Motor Off</a></button>
   </CENTER> 
