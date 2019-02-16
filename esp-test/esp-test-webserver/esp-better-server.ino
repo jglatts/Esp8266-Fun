f0a27f4503af51f6f02fe189afecb6789bb7a0aa
John Glatts <33476958+jglatts@users.noreply.github.com>
2019-02-16 10:24:18 -0500

    Update esp-better-server.ino

--- esp-test/esp-test-webserver/esp-better-server.ino
+++ esp-test/esp-test-webserver/esp-better-server.ino
@@ -26,11 +26,6 @@
    ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-   ToDo:
-        - Add data from motor and PIXEL to update on server
-        - Test
-
 */
 #include <Adafruit_NeoPixel.h>
 #include <ESP8266WiFi.h>
@@ -38,18 +33,15 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
+#ifndef STASSID
+#define STASSID " "
+#define STAPSK  " "
+#endif
 #define PIXPIN 5
 #define NUM_PIX 12
-#define RELAY 4
-#ifndef STASSID
-#define STASSID "your-ssid"
-#define STAPSK  "your-password"
-#endif
-
 
 const char *ssid = STASSID;
 const char *password = STAPSK;
-
 
 ESP8266WebServer server(80);
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIX, PIXPIN, NEO_GRB + NEO_KHZ800);
@@ -60,9 +52,6 @@ void handleRoot() {
   int sec = millis() / 1000;
   int min = sec / 60;
   int hr = min / 60;
-
-  // make sure relay is off
-  digitalWrite(RELAY, LOW);
 
   snprintf(temp, 400,
 
@@ -76,12 +65,13 @@ void handleRoot() {
   </head>\
   <body>\
     <h1>Hello from ESP8266!</h1>\
-    <button><a href=\"/PixOn\">Pixel On</a></button>\
-    <button><a href=\"/MotorOn\">Start Motor</a></button>\
     <p>Uptime: %02d:%02d:%02d</p>\
     <img src=\"/test.svg\" />\
   </body>\
-</html>", hr, min % 60, sec % 60 );
+</html>",
+
+           hr, min % 60, sec % 60
+          );
   server.send(200, "text/html", temp);
 }
 
@@ -101,18 +91,11 @@ void handleNotFound() {
   }
 
   server.send(404, "text/plain", message);
-  digitalWrite(led, 0);
 }
 
 void setup(void) {
-  // setup relay
-  pinMode(RELAY, OUTPUT);
-  digtialWrite(RELAY, LOW);
-
-  // start with PIXEL off
-  pixels.begin()
-  pixels.show()
-
+  pixels.begin();
+  pixels.show();
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -122,7 +105,6 @@ void setup(void) {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-    Serial.println("Not Connected");
   }
 
   Serial.println("");
@@ -137,7 +119,7 @@ void setup(void) {
 
   server.on("/", handleRoot);
   server.on("/PixOn", flashLED);
-  server.on("/MotorOn", moveMotor);
+  server.on("/PixOff", offLED);
   server.on("/test.svg", drawGraph);
   server.on("/inline", []() {
     server.send(200, "text/plain", "this works as well");
@@ -155,74 +137,91 @@ void loop(void) {
 
 
 void flashLED() {
-    char temp[400];
-    int cycles = 0;
-    String color; // wire and display what color is on
-
-    // make sure motor is off
-    digitalWrite(RELAY, LOW);
-
-    // setup html first
-    snprintf(temp, 400,
+      char temp_pix[500];
+      String color= "";
+      int cycles = 0;
+      
+      // setup html first
+      snprintf(temp_pix, 500,
              "<html>\
              <head>\
-                <meta http-equiv='refresh' content='5'/>\
+                <meta http-equiv='refresh' content='3'/>\
                 <title>ESP8266 Demo</title>\
                 <style>\
-                    body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
+                    body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088;}\
+                    p {size: 2em;}\
                 </style>\
                 </head>\
              <body>\
                 <h1>Hello from ESP8266!</h1>\
-                <button><a href=\"/PixOn\">Pixel On</a></button>\
-                <button><a href=\"/MotorOn\">Start Motor</a></button>\
                 <p>PIXEL CYCLES: %d</p>\
-                <p>CURRENT PIXEL COLOR: %s</p>\
-                <img src=\"/test.svg\" />\
+                <p><a href=\"/PixOff\">Pixel Off</a></p>\
              </body>\
-             </html>", hr, min % 60, sec % 60 );
+             </html>", cycles);
 
-    server.send(200, "text/html", temp);
-    // endless cycles when href is active
-    for (int j = 0;; ++j) {
+    server.send(200, "text/html", temp_pix);
+    // 5 cycles when href is active, testing what cycle time is the best
+    for (int j = 0;j < 5; ++j) {
         // PIX On
         for (int i = 0; i < NUM_PIX; ++i) {
             pixels.setPixelColor(i, pixels.Color(255,155,155));
             pixels.show();
             color = "Blue";
         }
-        delay(500);
+        delay(200);
         // PIX Off
         for (int x = 0; x < NUM_PIX; ++x) {
             pixels.setPixelColor(x, pixels.Color(0,0,0));
             pixels.show();
             color = "Off";
         }
-        delay(500);  // flash every half second
-        cycles++;   // increase cycle value after every off/on
+        delay(200);  // flash every half second
     }
+     cycles++;   // increase cycle value after every off/on
+}
+
+
+void offLED() {
+      char temp_pix[500];
+      
+      // setup html first
+      snprintf(temp_pix, 500,
+             "<html>\
+             <head>\
+                <meta http-equiv='refresh' content='3'/>\
+                <title>ESP8266 Demo</title>\
+                <style>\
+                    body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088;}\
+                    p {size: 2em;}\
+                </style>\
+                </head>\
+             <body>\
+                <h1>Hello from ESP8266!</h1>\
+                <p><a href=\"/PixOn\">Pixel On</a></p>\
+             </body>\
+             </html>");
+             
+             for (int i = 0; i < NUM_PIX; ++i) {
+                pixels.setPixelColor(i, pixels.Color(0,0,0));
+                pixels.show();
+             }
+             
+        server.send(200, "image/svg+xml", temp_pix);
 }
 
 
 void drawGraph() {
-   String out = "";
-   char temp[100];
-
    // Make sure pixel is off
     for (int i = 0; i < NUM_PIX; ++i) {
-        pixels.setPixelColor(x, pixels.Color(0,0,0));
+        pixels.setPixelColor(i, pixels.Color(0,0,0));
         pixels.show();
-   }
-
-  // make sure motor is off
-  digitalWrite(RELAY, HIGH);
-
-  // delay(600); delay just in case the off sequence doesnt work
+    }
+  String out = "";
+  char temp[100];
   out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
   out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
   out += "<g stroke=\"black\">\n";
   int y = rand() % 130;
-  // get values for graph
   for (int x = 10; x < 390; x += 10) {
     int y2 = rand() % 130;
     sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
@@ -232,33 +231,4 @@ void drawGraph() {
   out += "</g>\n</svg>\n";
 
   server.send(200, "image/svg+xml", out);
-}
-
-
-/* move the motor and update sever */
-void moveMotor() {
-    char temp[400];
-    int cycles = 0;
-    String motor_state; // wire and display what color is on
-    // setup html first
-    snprintf(temp, 400,
-             "<html>\
-             <head>\
-                <meta http-equiv='refresh' content='5'/>\
-                <title>ESP8266 Demo</title>\
-                <style>\
-                    body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
-                </style>\
-                </head>\
-             <body>\
-                <h1>Hello from ESP8266!</h1>\
-                <button><a href=\"/PixOn\">Pixel On</a></button>\
-                <button><a href=\"/MotorOn\">Start Motor</a></button>\
-                <p>MOTOR STATE: %s</p>\
-                <img src=\"/test.svg\" />\
-             </body>\
-             </html>", motor_state );
-
-    server.send(200, "text/html", temp);
-    digitalWrite(RELAY, HIGH);  // turn on motor
 }
