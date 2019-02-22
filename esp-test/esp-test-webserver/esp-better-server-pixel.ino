8636ddfffb1a291fa2e73ef183789dddd5071c82
John Glatts <33476958+jglatts@users.noreply.github.com>
2019-02-22 00:06:21 -0500

    improved distance display, but still more to be done

--- esp-test/esp-test-webserver/esp-better-server-pixel.ino
+++ esp-test/esp-test-webserver/esp-better-server-pixel.ino
@@ -2,6 +2,8 @@
    Copyright (c) 2015, Majenko Technologies
    All rights reserved.
 
+   Author: John Glatts
+
    Redistribution and use in source and binary forms, with or without modification,
    are permitted provided that the following conditions are met:
 
@@ -26,6 +28,7 @@
    ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+   
 */
 #include <Adafruit_NeoPixel.h>
 #include <ESP8266WiFi.h>
@@ -33,213 +36,286 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
+
 #ifndef STASSID
-#define STASSID " "
-#define STAPSK  " "
+#define STASSID ""
+#define STAPSK  ""
 #endif
 #define PIXPIN 5
+#define TRIG 4
+#define ECHO 0
 #define NUM_PIX 12
+
 
 const char *ssid = STASSID;
 const char *password = STAPSK;
 
-int cycles = 0;
+
+int cycles = 0; // put this in ledFlash() so it restarts at 0
+int duration;
+int distance = 0;
+String dist_pix_color = "";
+
 
 ESP8266WebServer server(80);
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIX, PIXPIN, NEO_GRB + NEO_KHZ800);
 
 
+// setup HTML pages as strings
+const char* index_html = "<!DOCTYPE html>"
+                         "<html>"
+                         "<head>"
+                         "<style>"
+                         "p {"
+                         "font-size: 3em;"
+                         "}"
+                         "a {"
+                         "font-size: 4em;"
+                         "}"
+                         "body {"
+                         "background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088;"
+                         "}"
+                         "</style>"
+                         "</head>"
+                         "<body>"
+                         "<p>JDG ESP SERVER</p>"
+                         "<p>Please select a button bellow.</p>"
+                         "<a href=\"/PixOn\">Pixel On</a>"
+                         "</br>"
+                         "<a href=\"/Distance\">Get Distance</a>"
+                         "</body>"
+                         "</html>";
+
+
+const char* pix_on_html = "<!DOCTYPE html>"
+                          "<html>"
+                          "<head>"
+                          "<meta http-equiv='refresh' content='3'/>"
+                          "<style>"
+                          "p {"
+                          "font-size: 3em;"
+                          "}"
+                          "a {"
+                          "font-size: 4em;"
+                          "}"
+                          "body {"
+                          "background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088;"
+                          "}"
+                          "</style>"
+                          "</head>"
+                          "<body>"
+                          "<p>JDG ESP SERVER</p>"
+                          "<p>Please select a button bellow.</p>"
+                          "<a href=\"/PixOff\">Pixel Off</a>"
+                          "</br>"
+                          "<a href=\"/Distance\">Get Distance</a>"
+                          "</body>"
+                          "</html>";
+
+
+const char* pix_off_html = "<!DOCTYPE html>"
+                           "<html>"
+                           "<head>"
+                           "<style>"
+                           "p {"
+                           "font-size: 3em;"
+                           "}"
+                           "a {"
+                           "font-size: 4em;"
+                           "}"
+                           "body {"
+                           "background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088;"
+                           "}"
+                           "</style>"
+                           "</head>"
+                           "<body>"
+                           "<p>JDG ESP SERVER</p>"
+                           "<p>Please select a button bellow.</p>"
+                           "<p>Pixel is OFF</p>"
+                           "<a href=\"/PixOff\">Pixel On</a>"
+                           "</br>"
+                           "<a href=\"/Distance\">Get Distance</a>"
+                           "</body>"
+                           "</html>";
+
+
+/* Landing Page */
 void handleRoot() {
-  char temp[400];
-  int sec = millis() / 1000;
-  int min = sec / 60;
-  int hr = min / 60;
-
-  snprintf(temp, 400,
-
-           "<html>\
-  <head>\
-    <meta http-equiv='refresh' content='5'/>\
-    <title>ESP8266 Demo</title>\
-    <style>\
-      body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
-      p {font-size:2em;}\
-      h2 {font-size:5em;}\
-    </style>\
-  </head>\
-  <body>\
-    <h1>Hello from ESP8266!</h1>\
-    <p>Uptime: %02d:%02d:%02d</p>\
-    <img src=\"/test.svg\" />\
-    <h2><a href='PixOn'>Pixel On</a></h2>\
-  </body>\
-</html>", hr, min % 60, sec % 60);
- server.send(200, "text/html", temp);
+    server.send(200, "text/html", index_html);
 }
 
 
 void handleNotFound() {
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
+    String message = "File Not Found\n\n";
+    message += "URI: ";
+    message += server.uri();
+    message += "\nMethod: ";
+    message += (server.method() == HTTP_GET) ? "GET" : "POST";
+    message += "\nArguments: ";
+    message += server.args();
+    message += "\n";
 
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
+    for (uint8_t i = 0; i < server.args(); i++) {
+        message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+    }
 
-  server.send(404, "text/plain", message);
+    server.send(404, "text/plain", message);
 }
 
+
+/* Setup Everything Dawg */
 void setup(void) {
-  pixels.begin();
-  pixels.show();
-  Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("");
+    pinMode(TRIG, OUTPUT);
+    pinMode(ECHO, INPUT);
+    pixels.begin();
+    pixels.show();
+    Serial.begin(115200);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    Serial.println("");
 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+    // Wait for connection
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
 
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 
-  if (MDNS.begin("esp8266")) {
-    Serial.println("MDNS responder started");
-  }
+    if (MDNS.begin("esp8266")) {
+        Serial.println("MDNS responder started");
+    }
 
-  server.on("/", handleRoot);
-  server.on("/PixOn", flashLED);
-  server.on("/PixOff", offLED);
-  server.on("/test.svg", drawGraph);
-  server.on("/inline", []() {
-    server.send(200, "text/plain", "this works as well");
-  });
-  server.onNotFound(handleNotFound);
-  server.begin();
-  Serial.println("HTTP server started");
+    server.on("/", handleRoot);
+    server.on("/PixOn", flashLED);
+    server.on("/PixOff", offLED);
+    server.on("/Distance", getDistance);
+    server.on("/inline", []() {
+        server.send(200, "text/plain", "this works as well");
+    });
+    server.onNotFound(handleNotFound);
+    server.begin();
+    Serial.println("HTTP server started");
 }
 
 
+/* Keep the server up and update it */
 void loop(void) {
-  server.handleClient();
-  MDNS.update();
+    server.handleClient();
+    MDNS.update();
 }
 
 
+/* Flash the PIXEL and display cycle count */
 void flashLED() {
-      char temp_pix[500];
-      String color= "";
-      
-      // setup html first
-      snprintf(temp_pix, 500,
-             "<html>\
-             <head>\
-                <meta http-equiv='refresh' content='3'/>\
-                <title>ESP8266 Demo</title>\
-                <style>\
-                    body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088;}\
-                    p {font-size: 4em;}\
-                </style>\
-                </head>\
-             <body>\
-                <h1>Hello from ESP8266!</h1>\
-                <p>Pixel Cycle: %d</p>\
-                <p><a href='PixOff'>Pixel Off</h2>\
-             </body>\
-             </html>", cycles);
+    String color= "";
 
-    server.send(200, "text/html", temp_pix);
+    server.send(200, "text/html", pix_on_html);
     // 5 cycles when href is active, testing what cycle time is the best
     for (int j = 0;j < 24; ++j) {
         // PIX On and switch Color
-       if (j%2==0) {
-           for (int i = 0; i < NUM_PIX; ++i) {
-              pixels.setPixelColor(i, pixels.Color(0,0,155));
-              pixels.show();
-              color = "Blue";
-         } 
-       } else {
+        if (j%2==0) {
+            for (int i = 0; i < NUM_PIX; ++i) {
+                pixels.setPixelColor(i, pixels.Color(0,0,155));
+                pixels.show();
+                color = "Blue";
+            }
+        } else {
             for (int q = 0; q < NUM_PIX; ++q) {
-              // testing the 'running' cycle
-              pixels.setPixelColor(q, pixels.Color(255,0,0));
-              pixels.show();
-              color = "Blue";
+                // testing the 'running' cycle
+                pixels.setPixelColor(q, pixels.Color(255,0,0));
+                pixels.show();
+                color = "Blue";
+            }
         }
-       }
-       delay(25);
+        delay(25);
         // PIX Off
         for (int x = 0; x < NUM_PIX; ++x) {
             pixels.setPixelColor(x, pixels.Color(0,0,0));
             pixels.show();
             color = "Off";
         }
-        delay(10);  
+        delay(25);
     }
-     cycles++;   // increase cycle value after every off/on
+    cycles++;   // increase cycle value after every off/on
 }
 
 
+/* Turn the PIXEL off */
 void offLED() {
-      char temp_pix[500];
-      
-      // setup html first
-      snprintf(temp_pix, 500,
-             "<html>\
-             <head>\
-                <meta http-equiv='refresh' content='3'/>\
-                <title>ESP8266 Demo</title>\
-                <style>\
-                    body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088;}\
-                    p {font-size: 4em;}\
-                    #test {font-size: 5em;}\
-                </style>\
-                </head>\
-             <body>\
-                <h1>Hello from ESP8266!</h1>\
-                <p><a href='PixOn'>Pixel On</a></h2>\
-             </body>\
-             </html>");
-             
-             for (int i = 0; i < NUM_PIX; ++i) {
-                pixels.setPixelColor(i, pixels.Color(0,0,0));
-                pixels.show();
-             }
-             
-        server.send(200, "text/html", temp_pix);
-}
-
-
-void drawGraph() {
-   // Make sure pixel is off
     for (int i = 0; i < NUM_PIX; ++i) {
         pixels.setPixelColor(i, pixels.Color(0,0,0));
         pixels.show();
     }
-  String out = "";
-  char temp[100];
-  out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
-  out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
-  out += "<g stroke=\"black\">\n";
-  int y = rand() % 130;
-  for (int x = 10; x < 390; x += 10) {
-    int y2 = rand() % 130;
-    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
-    out += temp;
-    y = y2;
-  }
-  out += "</g>\n</svg>\n";
-
-  server.send(200, "image/svg+xml", out);
+    server.send(200, "text/html", pix_off_html);
 }
+
+
+/* Activate the HC-SR04 and update the distance */
+void getDistance() {
+    for (int i = 0; i < NUM_PIX; ++i) {
+        pixels.setPixelColor(i, pixels.Color(0,0,0));
+        pixels.show();
+    }
+
+    digitalWrite(TRIG, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG, LOW);
+    // update distance
+    duration = pulseIn(ECHO, HIGH);
+    distance= duration * 0.034/2;
+    String color = "";
+    if (distance > NUM_PIX) {
+        for (int i = 0; i < NUM_PIX; ++i) {
+            pixels.setPixelColor(i, pixels.Color(0,0,0));
+            pixels.show();
+        }
+       color += "Off, No Harmful Objects";
+    }  else {
+        for (int i = 0; i < distance; ++i) {
+            pixels.setPixelColor(i, pixels.Color(255,0,0));  // red
+            pixels.show();
+        }
+       color += "RED, HARMFUL OBJECT";
+    }
+    String distance_html = "<!DOCTYPE html>";
+             distance_html +=                     "<html>";
+             distance_html +=                     "<head>";
+             distance_html +=                     "<meta http-equiv='refresh' content='3'/>";
+             distance_html +=                     "<style>";
+             distance_html +=                     "p {";
+             distance_html +=                     "font-size: 3em;";
+             distance_html +=                     "}";
+             distance_html +=                     "a {";
+             distance_html +=                     "font-size: 4em;";
+             distance_html +=                     "}";
+             distance_html +=                     "body {";
+             distance_html +=                     "background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088;";
+             distance_html +=                     "}";
+             distance_html +=                     "</style>";
+             distance_html +=                     "</head>";
+             distance_html +=                     "<body>";
+             distance_html +=                     "<p>JDG ESP SERVER</p>";
+             distance_html +=                     "<p>Please select a button bellow.</p>";
+             distance_html +=                     "<p>Current Distance: ";
+             distance_html +=                     distance;
+             distance_html +=                     "</p>";
+             distance_html +=                     "<p>PIXEL Color: ";
+             distance_html +=                     color;
+             distance_html +=                     "</p>";       
+             distance_html +=                     "<a href=\"/PixOn\">Pixel On</a>";
+             distance_html +=                     "</br>";
+             distance_html +=                     "<a href=\"/PixOff\">Pixel Off</a>";
+             distance_html +=                     "</body>";
+             distance_html +=                     "</html>";
+    
+    server.send(200, "text/html", distance_html);
+
+}
+
